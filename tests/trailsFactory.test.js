e7de8bdbd1319da457a606663ce7cf0c4f381807
pedro-ivo-molina <pedro.ivo.molina@gmail.com>
2020-05-26 23:16:30 -0300

    Added tests to buildSchedule

--- tests/trailsFactory.test.js
+++ tests/trailsFactory.test.js
@@ -1,28 +1,5 @@
 import trailsFactory from "../src/trailsFactory.js"
 
-let subject = [
-  {
-    name: "Meet1",
-    duration: 65
-  },
-  {
-    name: "Meet2",
-    duration: 240
-  },
-  {
-    name: "Meet3",
-    duration: 120
-  },
-  {
-    name: "Meet4",
-    duration: 30
-  },
-  {
-    name: "Meet5",
-    duration: 25
-  }
-]
-
 describe('#build', () => { 
   describe('success', () => {
     let subject = [
@@ -79,4 +56,45 @@ describe('#build', () => {
       expect(response).toThrow(Error)
     })
   })*/
+})
+
+describe('#buildSchedule', () => {
+  let subject = [
+    {
+      name: "Meet1",
+      duration: 65
+    },
+    {
+      name: "Meet2",
+      duration: 240
+    },
+    {
+      name: "Meet3",
+      duration: 120
+    },
+    {
+      name: "Meet4",
+      duration: 30
+    },
+    {
+      name: "Meet5",
+      duration: 25
+    }
+  ]
+  it('should return a sanitized trail', () => {
+    const factory = trailsFactory(subject)
+    factory.build()
+    const sanitized_trails = factory.buildSchedule()
+
+    expect(sanitized_trails).toEqual(expect.arrayContaining([["9:00 Meet1 65min", "10:5 Meet4 30min", "10:35 Meet5 25min", "12:00 Almoço", "13:00 Meet2 240min", "17:00 Evento de Networking"], ["9:00 Meet3 120min", "12:00 Almoço", "13:00 Evento de Networking"]]))
+  })
+
+  it('should return a lunch time within the array', () => {
+    const factory = trailsFactory(subject)
+    factory.build()
+    const sanitized_trails = factory.buildSchedule()
+    
+    expect(sanitized_trails[0]).toContain('12:00 Almoço')
+    expect(sanitized_trails[1]).toContain('12:00 Almoço')
+  })
 })
