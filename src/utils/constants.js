be562c6378728a32324297c02a0314c4e5a7fbe2
Nicolas Jardim Nicolao <nnicolao@uolinc.com>
2020-06-14 23:36:20 -0300

    add categories to constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,10 @@
 export const MORNING_MINUTES_LIMIT = 180
 export const EVENING_MINUTES_LIMIT = 240
 export const MAX_MINUTES_PER_TRAIL = 420
+
+export const CATEGORIES = {
+  FRONTEND: 'Frontend',
+  BACKEND: 'Backend',
+  ADVANCED_TOPICS: 'Advanced Topics',
+  TEST: 'Test'
+}
