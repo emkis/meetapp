9ae4a21f23a79722128b2fa41def925c0ad778d1
Emkis <nicolasemkis@gmail.com>
2020-06-17 03:18:10 -0300

    add vuelidate

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 
 import App from './App'
 import router from './router'
 import store from './store'
 
 import '@/styles/global.scss'
+
+Vue.use(Vuelidate)
 
 Vue.config.productionTip = false
 
