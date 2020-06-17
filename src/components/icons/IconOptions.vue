4b645a8f8ce9c9e4a504b686923ba07fc430924b
Emkis <nicolasemkis@gmail.com>
2020-06-17 02:39:24 -0300

    add new icons

---
+++ src/components/icons/IconOptions.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    :stroke="color"
+    fill="none"
+    stroke-width="0"
+    viewBox="0 0 35 27"
+    :height="size"
+    :width="size"
+    xmlns="http://www.w3.org/2000/svg"
+    role="presentation"
+  >
+    <circle cx="8" cy="13" r="2" stroke-width="2" />
+    <circle cx="17" cy="13" r="2" stroke-width="2" />
+    <circle cx="26" cy="13" r="2" stroke-width="2" />
+  </svg>
+</template>
+
+<script>
+import iconMixin from '@/mixins/iconMixin'
+export default {
+  mixins: [iconMixin],
+}
+</script>
--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,1 +1,4 @@
 export { default as IconOrganize } from './IconOrganize'
+export { default as IconOptions } from './IconOptions'
+export { default as IconClock } from './IconClock'
+export { default as IconPlus } from './IconPlus'
