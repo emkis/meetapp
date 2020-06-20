0ec10cf00a6101cbc8d806fd0787ec351477dd76
Emkis <nicolasemkis@gmail.com>
2020-06-20 00:53:13 -0300

    add component button confirm

---
+++ src/components/ButtonConfirm.vue
@@ -0,0 +1,62 @@
+<template>
+  <button
+    :class="{ 'button-confirm': clickedTimes >= 1 }"
+    @click="increaseClick"
+  >
+    <template>
+      <IconAttention v-if="clickedTimes >= 1" color="#fa5f1c" />
+      {{ currentLabel }}
+    </template>
+  </button>
+</template>
+
+<script>
+import { IconAttention } from '@/components/icons'
+
+export default {
+  name: 'ButtonConfirm',
+  components: { IconAttention },
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    confirmAction: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      clickedTimes: 0,
+    }
+  },
+  methods: {
+    increaseClick() {
+      this.clickedTimes = this.clickedTimes + 1
+    },
+  },
+  computed: {
+    currentLabel() {
+      return this.clickedTimes ? 'Click to confirm' : this.label
+    },
+  },
+  watch: {
+    clickedTimes(currentValue) {
+      if (currentValue === 2) {
+        this.confirmAction()
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.button-confirm {
+  color: #fa5f1c !important;
+
+  > svg {
+    margin: 0 2px -2px 0;
+  }
+}
+</style>
