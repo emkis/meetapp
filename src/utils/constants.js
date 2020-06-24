b8aae0bb70a0e547aae55e53dff3239af1d365dc
Emkis <nicolasemkis@gmail.com>
2020-06-24 03:41:11 -0300

    fix category color

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ export const EVENING_MINUTES_LIMIT = 240
 export const MAX_MINUTES_PER_TRAIL = 420
 
 export const CATEGORIES = [
-  { name: 'Advanced Topics', color: '#F6F4FF' },
+  { name: 'Advanced Topics', color: '#1A55AF' },
   { name: 'Beginner', color: '#FE9D2C' },
   { name: 'Frontend', color: '#F7DF1E' },
   { name: 'Backend', color: '#427A0A' },
