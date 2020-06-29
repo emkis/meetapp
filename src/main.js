fea226a16fb8b5e9f9512aea71d2cb7298a41302
Emkis <nicolasemkis@gmail.com>
2020-06-29 00:44:08 -0300

    add automatic global components registration

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import vClickOutside from 'v-click-outside'
 import App from './App'
 import router from './router'
 import store from './store'
+import globalRegistrationComponents from './utils/globalRegister'
 
 import '@/styles/global.scss'
 
 Vue.use(Vuelidate)
 Vue.use(vClickOutside)
 
+globalRegistrationComponents(Vue)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/utils/globalRegister.js
@@ -0,0 +1,36 @@
+import { upperFirst, camelCase } from 'lodash'
+
+/**
+ * Automatic Global Registration of Base Components
+ *
+ * More information on VueJS Docs
+ * https://vuejs.org/v2/guide/components-registration.html#Automatic-Global-Registration-of-Base-Components
+ */
+
+function globalComponentRegistration(VueInstance) {
+  const requireComponent = require.context(
+    '@/components/global',
+    false,
+    /Base[A-Z]\w+\.(vue|js)$/
+  )
+
+  requireComponent.keys().forEach(fileName => {
+    const componentConfig = requireComponent(fileName)
+
+    const componentName = upperFirst(
+      camelCase(
+        fileName
+          .split('/')
+          .pop()
+          .replace(/\.\w+$/, '')
+      )
+    )
+
+    VueInstance.component(
+      componentName,
+      componentConfig.default || componentConfig
+    )
+  })
+}
+
+export default globalComponentRegistration
