e7d1b1e54a03eac48c799e2697f72a8cf64deb40
emkis <nicolasemkis@gmail.com>
2020-07-15 04:17:15 -0300

    add components

---
+++ src/components/TrailColumn.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="trail-column">
+    <p class="title">Trail one</p>
+
+    <div class="scrollable">
+      <Meet
+        :key="index"
+        v-for="(meet, index) in meetsNumber"
+        :id="index"
+        title="Typescript Tips & Tricks"
+        category="Advanced Topics"
+        :schedule="{ start_time: '15:00', end_time: '15:30' }"
+        :hasOptions="false"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Meet from '@/components/Meet'
+
+export default {
+  name: 'TrailColumn',
+  components: { Meet },
+  data() {
+    return {
+      meetsNumber: 6,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.trail-column {
+  position: relative;
+  overflow: hidden;
+  margin-bottom: 30px;
+
+  .title {
+    margin-bottom: rem(10px);
+
+    font-size: rem(20px);
+    font-weight: 400;
+    line-height: 1.5;
+    color: var(--color-gray-dark);
+  }
+
+  .scrollable {
+    position: absolute;
+    top: 40px;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    overflow-y: auto;
+
+    .meet {
+      margin-right: 5px;
+    }
+  }
+}
+</style>
---
+++ src/components/TrailsBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="trails-board" v-dragscroll="true">
+    <TrailColumn />
+    <TrailColumn />
+    <TrailColumn />
+    <TrailColumn />
+    <TrailColumn />
+  </div>
+</template>
+
+<script>
+import { dragscroll } from 'vue-dragscroll'
+
+import TrailColumn from '@/components/TrailColumn'
+
+export default {
+  name: 'TrailsBoard',
+  components: { TrailColumn },
+  directives: { dragscroll },
+}
+</script>
+
+<style lang="scss" scoped>
+#trails-board {
+  $meet-width: rem(433px);
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax($meet-width, 1fr));
+  grid-auto-columns: minmax($meet-width, 1fr);
+  grid-auto-flow: column;
+  grid-gap: 40px;
+
+  overflow-x: auto;
+}
+</style>
