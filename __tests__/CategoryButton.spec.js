4395c962f0e3d97b68eeb35edeef4a53f160f66b
Nicolas Emkis <nicolasemkis@gmail.com>
2020-07-22 04:33:11 -0300

    change test to use mount

--- __tests__/CategoryButton.spec.js
+++ __tests__/CategoryButton.spec.js
@@ -1,18 +1,19 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import CategoryButton from '@/components/CategoryButton'
 
 describe('CategoryButton', () => {
   it('renders props.name inside component', () => {
     const name = 'Cool Category'
-    const wrapper = shallowMount(CategoryButton, {
+    const wrapper = mount(CategoryButton, {
       propsData: { name },
     })
+
     expect(wrapper.text()).toMatch(name)
   })
 
   test('if props.active is true, should have "active" class', () => {
     const name = 'Cool Category'
-    const wrapper = shallowMount(CategoryButton, {
+    const wrapper = mount(CategoryButton, {
       propsData: { name, active: true },
     })
 
