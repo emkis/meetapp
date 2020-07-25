f1a7aa160cec5428fe40567c14996bb67bf81b50
Nicolas Emkis <nicolasemkis@gmail.com>
2020-07-25 01:51:07 -0300

    add better test description

--- __tests__/CategoryButton.spec.js
+++ __tests__/CategoryButton.spec.js
@@ -2,21 +2,26 @@ import { mount } from '@vue/test-utils'
 import CategoryButton from '@/components/CategoryButton'
 
 describe('CategoryButton', () => {
-  it('renders props.name inside component', () => {
-    const name = 'Cool Category'
-    const wrapper = mount(CategoryButton, {
-      propsData: { name },
+  describe('Props', () => {
+    test('label | should render component with the passed-in text', () => {
+      const name = 'Cool Category'
+
+      const wrapper = mount(CategoryButton, { propsData: { name } })
+
+      expect(wrapper.text()).toMatch(name)
     })
 
-    expect(wrapper.text()).toMatch(name)
-  })
+    test('active | if is true, the component should have "active" class', () => {
+      const name = 'Cool Category'
 
-  test('if props.active is true, should have "active" class', () => {
-    const name = 'Cool Category'
-    const wrapper = mount(CategoryButton, {
-      propsData: { name, active: true },
+      const wrapper = mount(CategoryButton, {
+        propsData: {
+          name,
+          active: true,
+        },
+      })
+
+      expect(wrapper.classes()).toContain('active')
     })
-
-    expect(wrapper.classes()).toContain('active')
   })
 })
