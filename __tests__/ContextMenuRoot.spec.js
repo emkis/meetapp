b5cf15f1c2b808310c87561fc2de5b6d2d42417a
Nicolas Emkis <nicolasemkis@gmail.com>
2020-07-25 02:27:06 -0300

    add test

--- __tests__/ContextMenuRoot.spec.js
+++ __tests__/ContextMenuRoot.spec.js
@@ -4,21 +4,33 @@ import vClickOutside from 'v-click-outside'
 import ContextMenuRoot from '@/components/ContextMenuRoot'
 
 describe('ContextMenuRoot', () => {
-  const localVue = createLocalVue()
-  localVue.directive('click-outside', vClickOutside)
+  describe('Computed', () => {
+    const localVue = createLocalVue()
+    localVue.directive('click-outside', vClickOutside)
 
-  it('should render child component when contextMenu.isOpen is true', () => {
-    const mockComputed = {
-      isOpen: true,
-      options: [{ label: 'Option', action: () => {} }],
-      position: { x: 100, y: 400 },
-    }
+    it('contextMenu.isOpen | when is true should render child component with passed-in props', () => {
+      const mockComputed = {
+        isOpen: true,
+        options: [
+          { label: 'Option 1', action: () => {} },
+          { label: 'Option 2', action: () => {} },
+        ],
+        position: { x: 130, y: 210 },
+      }
 
-    const wrapper = mount(ContextMenuRoot, {
-      computed: { contextMenu: () => mockComputed },
-      localVue,
+      const wrapper = mount(ContextMenuRoot, {
+        computed: { contextMenu: () => mockComputed },
+        localVue,
+      })
+
+      const renderedOptions = wrapper
+        .findAll('.context-menu__option')
+        .wrappers.map((label) => label.text())
+
+      expect(wrapper.classes()).toContain('context-menu')
+      expect(wrapper.attributes('style')).toContain('top: 210px; left: 130px;')
+      expect(renderedOptions).toHaveLength(2)
+      expect(renderedOptions).toStrictEqual(['Option 1', 'Option 2'])
     })
-
-    expect(wrapper.html()).toContain('<div class="context-menu"')
   })
 })
