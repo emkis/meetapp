bfb5c6abac929a5de89cde1160f4c3681f052deb
Nicolas Emkis <nicolasemkis@gmail.com>
2020-07-29 00:21:50 -0300

    add meets module to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import category from '@/store/modules/category'
+import meets from '@/store/modules/meets'
 import contextMenu from '@/store/modules/contextMenu'
 
 Vue.use(Vuex)
@@ -9,6 +10,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     category,
+    meets,
     contextMenu,
   },
 })
