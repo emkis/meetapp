90ed624c1565b7aaa40da4a72d5cb57fc2f87ffa
emkis <nicolasemkis@gmail.com>
2020-08-28 21:41:54 -0300

    freeze constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,10 +2,10 @@ export const MORNING_MINUTES_LIMIT = 180
 export const EVENING_MINUTES_LIMIT = 240
 export const MAX_MINUTES_PER_TRAIL = 420
 
-export const CATEGORIES = [
+export const CATEGORIES = Object.freeze([
   { name: 'Advanced Topics', color: '#1A55AF' },
   { name: 'Beginner', color: '#FE9D2C' },
   { name: 'Frontend', color: '#F7DF1E' },
   { name: 'Backend', color: '#427A0A' },
   { name: 'Test', color: '#FA5F1C' },
-]
+])
