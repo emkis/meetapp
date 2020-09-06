e999813fb9d29f416444141cb6d6717e3d43f07f
emkis <nicolasemkis@gmail.com>
2020-09-06 04:40:25 -0300

    remove global register script

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
-import globalRegistrationComponents from './utils/globalRegister'
 
 import '@/styles/global.scss'
 
-globalRegistrationComponents(Vue)
 Vue.config.productionTip = false
 
 new Vue({
