a98e7b6881fd87d8ee99ecdddfb6508678141e3f
emkis <nicolasemkis@gmail.com>
2020-09-07 19:53:36 -0300

    add component

---
+++ src/components/EmptyMessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="empty-message"><slot /> <IconSadFace size="18" /></p>
+</template>
+
+<script>
+import { IconSadFace } from '@/components/icons'
+
+export default { components: { IconSadFace } }
+</script>
+
+<style lang="scss" scoped>
+.empty-message {
+  display: flex;
+  align-items: center;
+  color: var(--color-gray-dark);
+
+  > svg {
+    margin-left: 5px;
+  }
+}
+</style>
