f079593b9549e5c68fd293ca04875acad62bccf4
emkis <nicolasemkis@gmail.com>
2020-09-06 23:52:43 -0300

    move category constant to vuex module

--- src/store/modules/category/index.js
+++ src/store/modules/category/index.js
@@ -1,11 +1,13 @@
-import { CATEGORIES } from '@/utils/constants'
-
 export default {
   namespaced: true,
 
   state: {
-    categories: {
-      ...CATEGORIES,
-    },
+    categories: [
+      { name: 'Advanced Topics', color: '#1A55AF' },
+      { name: 'Beginner', color: '#FE9D2C' },
+      { name: 'Frontend', color: '#F7DF1E' },
+      { name: 'Backend', color: '#427A0A' },
+      { name: 'Test', color: '#FA5F1C' },
+    ],
   },
 }
