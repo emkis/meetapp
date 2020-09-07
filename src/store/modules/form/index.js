db44ea4b113776413cc4e65ac09ee16841409172
emkis <nicolasemkis@gmail.com>
2020-09-07 00:28:53 -0300

    refactor module & add new mutation

--- src/store/modules/form/index.js
+++ src/store/modules/form/index.js
@@ -1,27 +1,50 @@
-import { SET, UPDATE } from './types'
+import { SET, UPDATE, RESET } from './types'
+
+const initialFieldValues = {
+  id: null,
+  title: '',
+  category: '',
+  duration: null,
+}
 
 export default {
   namespaced: true,
 
   state: {
-    meet: {},
+    fields: { ...initialFieldValues },
+    isEditing: false,
   },
 
   mutations: {
-    [SET](state, meet) {
-      state.meet = { ...meet }
+    [SET](state, fieldValues) {
+      state.fields = { ...fieldValues }
+      state.isEditing = true
     },
-    [UPDATE](state, { fieldName, value }) {
-      state.meet[fieldName] = value
+    [UPDATE](state, updatedValues) {
+      state.fields = {
+        ...state.fields,
+        ...updatedValues,
+      }
+    },
+    [RESET](state) {
+      state.fields = { ...initialFieldValues }
+      state.isEditing = false
     },
   },
 
   actions: {
-    setMeet({ commit }, meet) {
-      commit(SET, meet)
+    setFields({ commit }, fields) {
+      commit(SET, fields)
     },
-    updateMeet({ commit }, newValues) {
-      commit(UPDATE, newValues)
+    reset({ commit }) {
+      commit(RESET)
+    },
+    updateField({ commit, dispatch, state }, updatedValues) {
+      commit(UPDATE, updatedValues)
+
+      if (state.isEditing) {
+        dispatch('meet/update', updatedValues, { root: true })
+      }
     },
   },
 }
