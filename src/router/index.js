838c2a9c76b9ae7467ab9fa219eb13169030a9ea
Nicolas Emkis <nicolasemkis@gmail.com>
2020-09-11 04:10:08 -0300

    pass props to page from router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import uuid from '@/utils/uuid'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -14,6 +16,46 @@ const routes = [
     path: '/trails',
     name: 'Trails',
     component: () => import(/* webpackChunkName: "Trails" */ '@/pages/Trails'),
+    props: () => {
+      return {
+        trails: [
+          {
+            id: uuid(),
+            meets: [
+              {
+                id: uuid(),
+                title: 'CSS Tips & Tricks',
+                category: 'Test',
+                schedule: { startTime: '10:00', endTime: '11:25' },
+              },
+              {
+                id: uuid(),
+                title: 'Frontend for dummmies',
+                category: 'Frontend',
+                schedule: { startTime: '14:00', endTime: '15:30' },
+              },
+              {
+                id: uuid(),
+                title: 'Vue.js Pro Tips',
+                category: 'Advanced Topics',
+                schedule: { startTime: '16:00', endTime: '16:45' },
+              },
+            ],
+          },
+          {
+            id: uuid(),
+            meets: [
+              {
+                id: uuid(),
+                title: 'Build an API in five minutes with Deno',
+                category: 'Backend',
+                duration: 65,
+              },
+            ],
+          },
+        ],
+      }
+    },
   },
   {
     path: '*',
