e61c2598dfdb481d248ba51e2797b1375ed3a3fb
emkis <nicolasemkis@gmail.com>
2020-09-27 23:57:56 -0300

    add event constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const MORNING_MINUTES_LIMIT = 180
 export const EVENING_MINUTES_LIMIT = 240
 export const MAX_MINUTES_PER_TRAIL = 420
+
+export const EVENT_TYPES = Object.freeze({ MEET: 'meet', BREAK: 'break' })
