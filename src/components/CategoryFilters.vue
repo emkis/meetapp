2068e352c76e81c5294a24c104c57f40f14a2024
emkis <nicolasemkis@gmail.com>
2020-10-02 02:23:09 -0300

    fix: only emit if state is not empty

--- src/components/CategoryFilters.vue
+++ src/components/CategoryFilters.vue
@@ -16,6 +16,7 @@
 
 <script>
 import { mapState, mapGetters } from 'vuex'
+import { isArrayEmpty } from '@/utils/validators'
 
 import CategoryButton from '@/components/CategoryButton'
 
@@ -55,7 +56,7 @@ export default {
   },
   watch: {
     selectedFilters(filters) {
-      this.getSelectedMeets(filters)
+      if (!isArrayEmpty(filters)) this.getSelectedMeets(filters)
     },
   },
 }
